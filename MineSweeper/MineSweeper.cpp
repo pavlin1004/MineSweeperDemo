267a10903776e15ebcd01fe5cb3645db08d92baf
pavlin.marinov4@gmail.com <pavlin.marinov4@gmail.com>
2021-12-15 18:08:36 +0200

    Added: Grid Generator GridGenerator Logic Completed

--- MineSweeper/MineSweeper.cpp
+++ MineSweeper/MineSweeper.cpp
@@ -2,6 +2,31 @@
 #include <stdlib.h>
 const int BOMBS_COUNT = 10;
 const int GRID_BORDERSIZE = 9;
+int NearbyBombCount(int grid[GRID_BORDERSIZE][GRID_BORDERSIZE], int coordinateX, int coordinateY)
+{
+    int elementValue = 0;
+    if (grid[coordinateX][coordinateY] != -1)
+    {
+        for (int i = coordinateX - 1;i < +coordinateX + 1;i++)
+        {
+            for (int j = coordinateY + 1;j <= coordinateY - 1;j--)
+            {
+                if (i >= 0 && j >= 0 && i < 9 && j <= 9)
+                {
+                    if (i == coordinateX && j == coordinateY) continue;
+                    else
+                    {
+                        if (grid[i][j] == -1)
+                        {
+                            elementValue++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return elementValue;
+}
 void GenerateBombs(int bombCoordinates[BOMBS_COUNT],int grid[GRID_BORDERSIZE][GRID_BORDERSIZE])
 {
     int currentCoordinates;   
@@ -28,15 +53,25 @@ void GenerateBombs(int bombCoordinates[BOMBS_COUNT],int grid[GRID_BORDERSIZE][GR
         grid[bombCoordinates[i] / 10][bombCoordinates[i] % 10] = -1;
     }
 }
-void GenerateGrid(int gird[GRID_BORDERSIZE][GRID_BORDERSIZE])
+void GenerateGrid(int grid[GRID_BORDERSIZE][GRID_BORDERSIZE], int bombCoordinates[BOMBS_COUNT])
+{
+    GenerateBombs(bombCoordinates, grid);
+    for (int i = 0;i < BOMBS_COUNT;i++)
+    {
+        for (int j = 0;j < BOMBS_COUNT;j++)
+        {
+            if (i != 0 && i != 9 && j != 0 && j != 9)
+            {
+                grid[i][j] = NearbyBombCount(grid, i, j);
+            }
+        }
+    }
+}
+void Reveal(int grid[GRID_BORDERSIZE][GRID_BORDERSIZE])
 {
     //TODO
 }
-void Reveal(int gird[GRID_BORDERSIZE][GRID_BORDERSIZE])
-{
-    //TODO
-}
-void Mark(int gird[GRID_BORDERSIZE][GRID_BORDERSIZE])
+void Mark(int grid[GRID_BORDERSIZE][GRID_BORDERSIZE])
 {
     //TODO
 }
